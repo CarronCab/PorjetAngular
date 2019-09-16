ef19f7ec01ea316eb2834b7ad730c91db4de2e13
CarronCab <charles.carroncabaret@gmail.com>
2019-09-16 15:02:56 +0200

    sidenav in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import {MatSidenavModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    SidenavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
