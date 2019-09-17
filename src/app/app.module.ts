263d3dabe42e0feefee06adecf29f277815edb35
HukanloS <hukanlos44@gmail.com>
2019-09-17 09:13:41 +0200

    Remplacement de " par '

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './detail/detail.component';
-import { HeaderComponent } from './header/header.component';
 import { GenericDisplayComponent } from './generic-display/generic-display.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BestRatedFilmsComponent } from './best-rated-films/best-rated-films.component';
@@ -33,6 +33,7 @@ import {
 @NgModule({
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
     MatCheckboxModule,
     AppRoutingModule,
@@ -56,7 +57,6 @@ import {
     AppComponent,
     HomeComponent,
     DetailComponent,
-    HeaderComponent,
     GenericDisplayComponent,
     PageNotFoundComponent,
     BestRatedFilmsComponent,
