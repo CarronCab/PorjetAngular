72528302fb34560d8f8d8ba439e5f266ec3a9f26
HukanloS <hukanlos44@gmail.com>
2019-09-17 15:23:06 +0200

    "" become ''

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HomeComponent } from "./home/home.component";
-import { DetailComponent } from "./detail/detail.component";
-import { GenericDisplayComponent } from "./generic-display/generic-display.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { BestRatedFilmsComponent } from "./best-rated-films/best-rated-films.component";
-import { UpcomingFilmsComponent } from "./upcoming-films/upcoming-films.component";
-import { NowPlayingFilmsComponent } from "./now-playing-films/now-playing-films.component";
-import { PopularFilmsComponent } from "./popular-films/popular-films.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { GenericDisplayComponent } from './generic-display/generic-display.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BestRatedFilmsComponent } from './best-rated-films/best-rated-films.component';
+import { UpcomingFilmsComponent } from './upcoming-films/upcoming-films.component';
+import { NowPlayingFilmsComponent } from './now-playing-films/now-playing-films.component';
+import { PopularFilmsComponent } from './popular-films/popular-films.component';
 import {
   MatAutocompleteModule,
   MatCheckboxModule,
@@ -29,8 +29,8 @@ import {
   MatIconModule,
   MatTooltipModule,
   MatLineModule
-} from "@angular/material";
-import { HttpClientModule } from "@angular/common/http";
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
